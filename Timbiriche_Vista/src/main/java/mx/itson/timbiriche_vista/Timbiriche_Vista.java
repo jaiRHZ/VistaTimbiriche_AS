a43c67c1a9bceca4205937549b081805a0d8dc5a
jaiRHZ <133193972+jaiRHZ@users.noreply.github.com>
2023-11-08 21:01:04 -0700

    Creacion de Tablero
    Creacion de la pantalla tablero y la pantalla de configuracion

---
+++ Timbiriche_Vista/src/main/java/mx/itson/timbiriche_vista/Timbiriche_Vista.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.timbiriche_vista;
+
+/**
+ *
+ * @author Jairo G. Rodriguez Hernandez 00000213248
+ */
+public class Timbiriche_Vista {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
