053265311e5db611c5ceff228d62e0c8100c1e29
jaiRHZ <133193972+jaiRHZ@users.noreply.github.com>
2023-11-12 00:52:28 -0700

    Vista Maquetada
    La vista ha sido totalmente maqueteda

--- Timbiriche_Vista/src/main/java/vista/Configuracion.java
+++ Timbiriche_Vista/src/main/java/vista/Configuracion.java
@@ -33,10 +33,22 @@ public class Configuracion extends javax.swing.JFrame {
 
         btnCancelar = new javax.swing.JButton();
         btnAceptar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        nombreJ1 = new javax.swing.JLabel();
+        nombreJ2 = new javax.swing.JLabel();
+        nombreJ3 = new javax.swing.JLabel();
+        nombreJ4 = new javax.swing.JLabel();
+        imgJugador1 = new javax.swing.JLabel();
+        imgJugador2 = new javax.swing.JLabel();
+        imgJugador3 = new javax.swing.JLabel();
+        imgJugador4 = new javax.swing.JLabel();
+        btnColorJ1 = new javax.swing.JButton();
+        btnColorJ2 = new javax.swing.JButton();
+        btnColorJ3 = new javax.swing.JButton();
+        btnColorJ4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Configuracion del Tablero");
-        setPreferredSize(new java.awt.Dimension(500, 400));
         setResizable(false);
 
         btnCancelar.setBackground(new java.awt.Color(248, 20, 19));
@@ -52,21 +64,104 @@ public class Configuracion extends javax.swing.JFrame {
         btnAceptar.setText("Aceptar");
         btnAceptar.setPreferredSize(new java.awt.Dimension(90, 50));
 
+        jLabel1.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
+        jLabel1.setText("Cambiar Color");
+
+        nombreJ1.setText("Jugador 1");
+
+        nombreJ2.setText("Jugador 2");
+
+        nombreJ3.setText("Jugador 3");
+
+        nombreJ4.setText("Jugador 3");
+
+        imgJugador1.setText("img1");
+        imgJugador1.setPreferredSize(new java.awt.Dimension(100, 120));
+
+        imgJugador2.setText("img2");
+        imgJugador2.setPreferredSize(new java.awt.Dimension(100, 120));
+
+        imgJugador3.setText("img3");
+        imgJugador3.setPreferredSize(new java.awt.Dimension(100, 120));
+
+        imgJugador4.setText("img4");
+        imgJugador4.setPreferredSize(new java.awt.Dimension(100, 120));
+
+        btnColorJ1.setText("...");
+
+        btnColorJ2.setText("...");
+
+        btnColorJ3.setText("...");
+
+        btnColorJ4.setText("...");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(117, 117, 117)
-                .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(69, 69, 69)
-                .addComponent(btnAceptar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(134, Short.MAX_VALUE))
+                .addGap(87, 87, 87)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                    .addComponent(nombreJ1)
+                    .addComponent(imgJugador1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(imgJugador3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnColorJ1)
+                    .addComponent(btnColorJ3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 46, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                    .addComponent(nombreJ2)
+                    .addComponent(imgJugador2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(imgJugador4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnColorJ2)
+                    .addComponent(btnColorJ4))
+                .addContainerGap(67, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(162, 162, 162)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(117, 117, 117)
+                        .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(69, 69, 69)
+                        .addComponent(btnAceptar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(109, 109, 109)
+                        .addComponent(nombreJ3)
+                        .addGap(91, 91, 91)
+                        .addComponent(nombreJ4)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(308, Short.MAX_VALUE)
+                .addGap(31, 31, 31)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(imgJugador1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(imgJugador2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnColorJ2)
+                    .addComponent(btnColorJ1))
+                .addGap(56, 56, 56)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(nombreJ1)
+                    .addComponent(nombreJ2))
+                .addGap(55, 55, 55)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(imgJugador3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(imgJugador4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnColorJ3)
+                    .addComponent(btnColorJ4))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(nombreJ3)
+                    .addComponent(nombreJ4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(btnAceptar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -120,5 +215,18 @@ public class Configuracion extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAceptar;
     private javax.swing.JButton btnCancelar;
+    private javax.swing.JButton btnColorJ1;
+    private javax.swing.JButton btnColorJ2;
+    private javax.swing.JButton btnColorJ3;
+    private javax.swing.JButton btnColorJ4;
+    public javax.swing.JLabel imgJugador1;
+    public javax.swing.JLabel imgJugador2;
+    public javax.swing.JLabel imgJugador3;
+    public javax.swing.JLabel imgJugador4;
+    private javax.swing.JLabel jLabel1;
+    public javax.swing.JLabel nombreJ1;
+    public javax.swing.JLabel nombreJ2;
+    public javax.swing.JLabel nombreJ3;
+    public javax.swing.JLabel nombreJ4;
     // End of variables declaration//GEN-END:variables
 }
